ed27b9d8b3b862412137f75b3efa26926debefe4
Josue Martinez <j.e.martinez1993@gmail.com>
2022-01-25 23:59:12 +0100

    finished reverse linked list, need some tests

---
+++ hardPython/linked_lists/reverse_linked_list.py
@@ -0,0 +1,21 @@
+from turtle import clear
+
+
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+def reverse_linked_list(head):
+    curr = head
+    previous = None
+    while True:
+        tmp_curr = curr.next
+        curr.next = previous
+        previous = curr
+        if tmp_curr == None:
+            return curr
+        else:
+            curr = tmp_curr
+
+
