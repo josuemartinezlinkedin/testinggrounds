75109f1fab56ab4d5241f376012b0c8e0872df8e
Josue Martinez <j.e.martinez1993@gmail.com>
2022-07-17 20:19:52 +0200

    adding OddOccurrencesInArray with 2 solutions

---
+++ jsProblems/OddOccurrencesInArray.js
@@ -0,0 +1,29 @@
+/*
+1st solution using XOR operator which is the fastest method
+
+like turning switches on and off, and when a pattern has already been displyed
+then leaves just the unique binary pattern
+a = 1  //001
+b = 2 //010
+ a ^ b = 3 // 011
+*/
+function solution1 (A) {
+    result = 0
+    for (let element of A ) {
+        result = result ^ element
+    }
+    return result;
+}
+
+//second solution more readable but sorting first then traversing array can be costly
+function solution2(A) {
+    A.sort()
+    if (A.length < 2) { return A[0] }
+    else {
+        for (i = 0; i < A.length; i++) {
+            if (A[i] !== A[i + 1] && A[i] !== A[i - 1]) {
+                return A[i]
+            }
+        }
+    }
+}
